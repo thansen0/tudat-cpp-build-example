1930dc0082dc9c73ae578e3067a2d89c38baee67
thomas <thomash1996@live.com>
2025-08-20 19:41:01 -0600

    added tudat example in code

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,38 @@
 #include <Eigen/Dense>
 #include <iostream>
-#include <stdlib.h>
 
-// Note: this doesn't use tudat code yet
+#include "tudat/io/basicInputOutput.h"
+#include "tudat/interface/spice/spiceInterface.h"
 
 using namespace std;
 
 int main() {
-    Eigen::Matrix<double, 6, 1> v;
-    v << 1, 2, 3, 4, 5, 6;
-    std::cout << v.transpose() << std::endl;
-    return 0;
+    cout << "Simple Tudat example" << endl;
 
+    try
+    {
+        // Load standard Spice kernels (de421.bsp etc.)
+        tudat::spice_interface::loadStandardSpiceKernels();
+
+        // Epoch in seconds since J2000 (e.g. 1 day after J2000)
+        double epoch = 86400.0;
+
+        // Get state of Earth relative to Sun at the epoch
+        Eigen::Vector6d earthState =
+            tudat::spice_interface::getBodyCartesianStateAtEpoch(
+                "Earth",            // Target
+                "Sun",              // Observer
+                "ECLIPJ2000",       // Reference frame
+                "NONE",             // Aberration corrections
+                epoch );            // Epoch
+
+        std::cout << "Earth state (x,y,z,vx,vy,vz):\n" << earthState << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
 
